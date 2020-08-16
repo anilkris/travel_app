62020a53f63e3ad831d7d26a60f621091bd0e482
anil <anilkrishna@gmail.com>
2020-08-16 22:07:04 +0530

    travel app

---
+++ lib/homepage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return Container(
+      height: 68.0,
+      width: 68.0,
+      decoration: BoxDecoration(
+          color: Theme.of(context).accentColor,
+          borderRadius: BorderRadius.circular(30)),
+      child: Icon(_icons[index],
+          size: 25.0, color: Theme.of(context).primaryColor),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: ListView(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 20.0, right: 120.0),
+          child: Text(
+            'What would you like to find?',
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 20.0),
+        _buildIcon(0)
+      ],
+    )));
+  }
+}
